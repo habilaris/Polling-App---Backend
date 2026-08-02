922c41a940801c23a29b96dbc90a51b838cbc45f
habilaris <habilaris4@gmail.com>
2026-08-03 01:35:37 +0500

    Ready for prod

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MIDDlEWARES
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(logger);
 
 // DB CONNECTION
